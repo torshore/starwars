00968b592a4fb2ee79d3b383dfb0c9cecd419f47
Tory Shoreman <tory.shoreman@beamworks.io>
2019-10-09 09:48:07 -0400

    Add CSS for basic components

--- src/PageLayout.jsx
+++ src/PageLayout.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { css } from 'emotion'
 
 function PageLayout(props) {
-    return <div className="page-layout">
-        Star Wars
-        {props.children}
+    return <div
+        className={css`
+            display: flex;
+            flex-direction: column;
+            height: 100%;
+        `}
+    >
+        <div className={css`
+            flex: none;
+            display: flex;
+            align-items: center;
+            padding: 10px 20px;
+            background: #163c8e;
+        `}>
+            <span>Star Wars</span>
+        </div>
+
+        <div className={css`
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            padding: 45px;
+            box-sizing: border-box;
+            height: 100%;
+        `}>
+            {props.children}
+        </div>
     </div>;
 }
 
