2a64a9c1a0e8eff5dd66694fb6b50cc4ae8ff656
Tory Shoreman <tory.shoreman@beamworks.io>
2019-10-10 16:07:12 -0400

    Display all data in table with styling

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,30 +1,79 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { css } from 'emotion';
+
+const headerCellCSS = css`
+    width: 25%;
+    border: none;
+    padding: 20px 10px;
+    text-align: left;
+    line-height: 16px;
+    font-size: 14px;
+    vertical-align: middle;
+`;
+
+const bodyCellCSS = css`
+    width: 25%;
+    padding: 15px 10px;
+    overflow: hidden;
+    line-height: 16px;
+    font-size: 12px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+`;
 
 function Table() {
     const characters = useSelector(state => state.characters);
     const ships = useSelector(state => state.ships);
-    const copyRowData = characters && characters.concat();
+    const rowData = characters && characters.concat();
     ships && ships.map((ship, shipIndex) => {
-        return copyRowData.splice((shipIndex + 1) * 3 - 1, 0, ship);
+        return rowData.splice((shipIndex + 1) * 3 - 1, 0, ship);
     });
 
-    return <div className="table">
-        <table>
-            <thead>
+    return <div className={css`
+        width: 100%;
+        height: 100%;
+    `}>
+        <table className={css`
+            flex: 1;
+            table-layout: fixed;
+            width: 100%;
+            height: 100%;
+            border-spacing: 0;
+            border-collapse: collapse;
+        `}>
+            <thead className={css`
+                border: 1px solid #656565;
+                border-radius: 10px;
+                background-color: #741018;
+            `}>
                 <tr>
-                    <th className="-tiny"></th>
-                    <th>Name</th>
-                    <th>BirthYear</th>
-                    <th>Height</th>
-                    <th>Mass</th>
-                    <th className="-tiny"></th>
+                    <th className={headerCellCSS}>Name</th>
+                    <th className={headerCellCSS}>Birth Year</th>
+                    <th className={headerCellCSS}>Height</th>
+                    <th className={headerCellCSS}>Mass</th>
                 </tr>
             </thead>
-            <tbody>
-                {copyRowData && copyRowData.map((row, rowIndex) =>
-                    <tr key={rowIndex}>
-                        <td>{row.name}</td>
+
+            <tbody className={css`
+                border: 1px solid #656565;
+                width: 100%;
+                height: 100%;
+            `}>
+                {rowData && rowData.map((row, rowIndex) =>
+                    <tr
+                        key={rowIndex}
+                        className={css`
+                            &[data-is-light=true] {
+                                background: #163c8e;
+                            }
+                        `}
+                        data-is-light={rowIndex % 2 === 0}
+                    >
+                        <td className={bodyCellCSS}>{row.name}</td>
+                        <td>{row.birth_year || ''}</td>
+                        <td>{row.height || ''}</td>
+                        <td>{row.mass || ''}</td>
                     </tr>
                 )}
             </tbody>
