18d3fb99b161ffe8857084b2260c28661e4661af
Tory Shoreman <tory.shoreman@beamworks.io>
2019-10-09 22:18:29 -0400

    Get characters in App using redux hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { getCharacters } from './actions/actions.js';
 import PageLayout from './PageLayout.jsx';
 import Table from './Table.jsx';
 
 import './App.css';
 
 function App() {
+    const characters = useSelector(state =>  state.characters);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getCharacters());
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
     return <div className="app">
         <Router>
             <PageLayout>
